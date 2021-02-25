369f21cc9f8300290fc94898be4ade5872e09037
Aruna Srinivasiah <17347140+aruna-srinivasiah@users.noreply.github.com>
2021-02-25 17:26:24 -0500

    changes

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@
 console.log("Your index.js file is loaded correctly!");
 
 // B. Create a jQuery Event Listener That Triggers a UI Interaction
+
+// $('body').load(function() {
+  //  $("backgroundImg").fadeIn();
+// }); 
+
+// $(document).ready(function() {
+   // $("inline-block").hover(){
+      //      $(this).css("background-color", "black");
+      //  }
+  //  })
+ // });
+
+    $(document).ready(function(){
+    $("a").hover(function(){
+        background-color: black; 
+    });
+  });
