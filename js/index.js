6e874e295f860614cf084ab918f525ec9f147662
Aruna Srinivasiah <17347140+aruna-srinivasiah@users.noreply.github.com>
2021-02-26 16:50:24 -0500

    Background changes

--- js/index.js
+++ js/index.js
@@ -14,8 +14,30 @@ console.log("Your index.js file is loaded correctly!");
   //  })
  // });
 
-    $(document).ready(function(){
+   /* $(document).ready(function(){
     $("a").hover(function(){
         background-color: black; 
     });
+  }); */
+
+  // $("#background").bind("load", function () { $(this).fadeIn(); });
+  $(document).ready(function() { 
+   $('.background').delay(1000).animate({'opacity':'1'},3000);
   });
+
+// $('.background').animate({'opacity':'1'},1000);
+
+// $(document).ready(function(){
+ // $("a").hover(function(){
+   //   background-color: black;
+  // }); 
+// }); 
+
+
+ // $('#background').delay(600).animate({'opacity':'1'},800);
+  //invoke background fadein on page load 
+  //  $(document).load(function() {      
+ //   $('#background').fadeIn("slow"); 
+    //fadeIn rate in milliseconds. 
+    //A higher number = slower fadeIn rate 
+  //  });  
